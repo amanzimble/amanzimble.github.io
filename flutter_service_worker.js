2a58223e18d3d68b216bd2a2a65a4a659fe558be
abubakar955786 <abu664034@gmail.com>
2024-01-17 17:45:30 +0530

    jan 17 2024

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "848289c526c7b3f7dd156cdf9573ff1b",
-"index.html": "580e561e1c5e08524ce39c4bf2ef693e",
-"/": "580e561e1c5e08524ce39c4bf2ef693e",
-"main.dart.js": "2c2e3bb4d58875ce591e8ba070150aba",
+"index.html": "3b7e3b464bca367d83347e54338265e1",
+"/": "3b7e3b464bca367d83347e54338265e1",
+"main.dart.js": "fa4f8774f46765a997d02d4c10756196",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
