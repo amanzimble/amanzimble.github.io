9944abcd30d5d12eca0dadc2ad3a80f16b7c0303
abubakar955786 <abu664034@gmail.com>
2024-01-28 01:33:39 +0530

    PWA Testing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "index.html": "786962bad31f1ef32e72b51f5b206656",
 "/": "786962bad31f1ef32e72b51f5b206656",
 "firebase-messaging-sw.js": "32f827c54327e28d9cbeba4806e70a96",
-"main.dart.js": "99e099206947537febd437288e081e89",
+"main.dart.js": "c0c33429e1762417c738bd028a709f2f",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
