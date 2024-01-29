9bae87f42b99d5cee9e89aac26a54fc4735fc322
abubakar955786 <abu664034@gmail.com>
2024-01-29 11:39:30 +0530

    PWA Testing in I Phhone

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "index.html": "786962bad31f1ef32e72b51f5b206656",
 "/": "786962bad31f1ef32e72b51f5b206656",
 "firebase-messaging-sw.js": "32f827c54327e28d9cbeba4806e70a96",
-"main.dart.js": "7673724e3be0b3588e222f8ea895a8cc",
+"main.dart.js": "b2e3e92c083c143e2c8fbbb82bd1cd82",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "7f9801cb8a190e5a039d83e562d89baf",
 "icons/favicon.ico": "d93f71529ec3e3ff12643fd7672fef9a",
