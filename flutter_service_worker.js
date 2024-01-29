917a5a7e94193815ec9ea65502693bd97021710e
abubakar955786 <abu664034@gmail.com>
2024-01-29 15:28:34 +0530

    FCM Testing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "index.html": "786962bad31f1ef32e72b51f5b206656",
 "/": "786962bad31f1ef32e72b51f5b206656",
 "firebase-messaging-sw.js": "32f827c54327e28d9cbeba4806e70a96",
-"main.dart.js": "3047ae65a232daefc1acbcc0d2778acc",
+"main.dart.js": "2436554595f6c8ab58d39a60e463d5d0",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "7f9801cb8a190e5a039d83e562d89baf",
 "icons/favicon.ico": "d93f71529ec3e3ff12643fd7672fef9a",
