4a21fb7b80342b1568c5dcf48c2506120115ab6e
abubakar955786 <abu664034@gmail.com>
2024-02-07 18:26:34 +0530

    Feb 07 2024

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "index.html": "5bc48d565cddfbdf46b1049ec669a083",
 "/": "5bc48d565cddfbdf46b1049ec669a083",
 "firebase-messaging-sw.js": "32f827c54327e28d9cbeba4806e70a96",
-"main.dart.js": "cc2105d484dac163e8013d3cbf625e59",
+"main.dart.js": "31a9e25c23da623ad8b55cdf4317d29c",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "7f9801cb8a190e5a039d83e562d89baf",
 "icons/favicon.ico": "d93f71529ec3e3ff12643fd7672fef9a",
