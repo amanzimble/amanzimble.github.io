84e3b3cc884b579d8f18934fa3e9da48b73f488a
Deviser <zimble@Ishaans-MacBook-Pro.local>
2024-03-15 12:16:58 +0530

    disable copy text

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "index.html": "5bc48d565cddfbdf46b1049ec669a083",
 "/": "5bc48d565cddfbdf46b1049ec669a083",
 "firebase-messaging-sw.js": "c4cae52db024f7917cde63d98bc274ad",
-"main.dart.js": "c8f7f3d7523fa7f090150bbbbb9dcaf5",
+"main.dart.js": "8feea57666d92843b60345af27a936b9",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "7f9801cb8a190e5a039d83e562d89baf",
 "icons/favicon.ico": "d93f71529ec3e3ff12643fd7672fef9a",
