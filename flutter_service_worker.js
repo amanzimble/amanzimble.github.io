6bd5b9105cb68b3c1edcbb6c9e3bd46af17c3ee2
Amanpreet <aman@zimblecode.com>
2024-03-20 17:38:56 +0530

    puffguys driver web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,12 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f6260428621008c29844593f1b9880b9",
-"index.html": "964125f0aac26ec10133e1d81a2dda2c",
-"/": "964125f0aac26ec10133e1d81a2dda2c",
+"index.html": "85d6911ec560aefb6fec884e939b62e9",
+"/": "85d6911ec560aefb6fec884e939b62e9",
 "firebase-messaging-sw.js": "c4cae52db024f7917cde63d98bc274ad",
-"main.dart.js": "979aa0070131e2e2ba6bee1570c6364c",
+"main.dart.js": "b1ad578bb9fe8266760eff4bc2e38536",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"favicon.png": "7f9801cb8a190e5a039d83e562d89baf",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
