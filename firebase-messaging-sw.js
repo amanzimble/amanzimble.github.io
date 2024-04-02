3edf16d4f7bde39ffab22bc49f4193ae9405f709
Amanpreet <aman@zimblecode.com>
2024-04-02 17:52:40 +0530

    puffguys driver wb app

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,70 @@
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
+/*importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 
-
+   *//*Update with yours config*//*
   const firebaseConfig = {
-      apiKey: "AIzaSyB_gb07rei8CG_tnvPR0iAtuUtnhBGbQNc",
-      authDomain: "puffguys-cb72d.firebaseapp.com",
-      projectId: "puffguys-cb72d",
-      storageBucket: "puffguys-cb72d.appspot.com",
-      messagingSenderId: "698976600856",
-      appId: "1:698976600856:web:cd0b419adde6cb3795d46d",
-      measurementId: "G-3JM33GZ5K8",
+    apiKey: "AIzaSyBLQFRBcbV5RVKraUu7OVH8ITGPU_cIJ4g",
+    authDomain: "flutterwebfcm.firebaseapp.com",
+    projectId: "flutterwebfcm",
+    storageBucket: "flutterwebfcm.appspot.com",
+    messagingSenderId: "948323628133",
+    appId: "1:948323628133:web:4fd8c6cac020b70f6cc28c",
+    measurementId: "G-EX8C45PXB7"
     };
   firebase.initializeApp(firebaseConfig);
-  const messaging = firebase.messaging();
+  const messaging = firebase.messaging();*/
 
 
-  messaging.onBackgroundMessage(function(payload) {
+
+
+
+
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLQFRBcbV5RVKraUu7OVH8ITGPU_cIJ4g",
+    authDomain: "flutterwebfcm.firebaseapp.com",
+    projectId: "flutterwebfcm",
+    storageBucket: "flutterwebfcm.appspot.com",
+    messagingSenderId: "948323628133",
+    appId: "1:948323628133:web:4fd8c6cac020b70f6cc28c",
+    measurementId: "G-EX8C45PXB7"
+    };
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+let darkPatternsValue = 0;
+
+self.addEventListener('message', function(event){
+  if (event.data && event.data.type === 'SET_DARK_PATTERNS_VALUE') {
+    darkPatternsValue = event.data.value;
+  }
+});
+
+messaging.onBackgroundMessage(function(payload) {
     console.log('Received background message ', payload);
+    if(darkPatternsValue != 1){
+      return;
+    }
+    lastNotificationId = currentNotificationId;
 
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
-      body: payload.notification.body,
-    };
+        body: payload.notification.body,
+      };
 
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
-  });
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification clicked ', event);
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/?source=notification')
+    );
+ });
