787b50e9c3cb84e6610734238f2a4d4ae655b458
Amanpreet <aman@zimblecode.com>
2024-04-02 16:10:57 +0530

    hide skip button

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,42 @@
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 
-
+   /*Update with yours config*/
   const firebaseConfig = {
-      apiKey: "AIzaSyB_gb07rei8CG_tnvPR0iAtuUtnhBGbQNc",
-      authDomain: "puffguys-cb72d.firebaseapp.com",
-      projectId: "puffguys-cb72d",
-      storageBucket: "puffguys-cb72d.appspot.com",
-      messagingSenderId: "698976600856",
-      appId: "1:698976600856:web:cd0b419adde6cb3795d46d",
-      measurementId: "G-3JM33GZ5K8",
+    apiKey: "AIzaSyBLQFRBcbV5RVKraUu7OVH8ITGPU_cIJ4g",
+    authDomain: "flutterwebfcm.firebaseapp.com",
+    projectId: "flutterwebfcm",
+    storageBucket: "flutterwebfcm.appspot.com",
+    messagingSenderId: "948323628133",
+    appId: "1:948323628133:web:4fd8c6cac020b70f6cc28c",
+    measurementId: "G-EX8C45PXB7"
     };
   firebase.initializeApp(firebaseConfig);
   const messaging = firebase.messaging();
 
+  /*messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);*/
+  // Necessary to receive background messages:
+  const messaging = firebase.messaging();
 
-  messaging.onBackgroundMessage(function(payload) {
-    console.log('Received background message ', payload);
+  // Optional:
+  messaging.onBackgroundMessage((payload) => {
+      console.log("onBackgroundMessage", payload);
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-    };
+      const { title, body, image } = payload.notification ?? {};
+      console.log("onBackgroundMessage title", title);
+      console.log("onBackgroundMessage body", body);
+      console.log("onBackgroundMessage image", image);
 
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
+      if (!title) {
+          return;
+      }
+      console.log("onBackgroundMessage Make sure macOS allow chrome to show notification from macOS settings/notifications/chrome");
+
+      self.registration.showNotification(title, {
+          body,
+          icon: image || '/icons/Icon-192.png',
+      });
   });
